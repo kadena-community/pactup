896cffaf57052baf74b9befd482a3b11c2d5783d
Salama Ashoush <salamaashoush@gmail.com>
2024-08-07 23:44:28 +0200

    fix: fix recursive version inference

--- src/default_version.rs
+++ src/default_version.rs
@@ -1,12 +1,16 @@
+use log::info;
+
 use crate::config::PactupConfig;
 use crate::version::Version;
 use std::str::FromStr;
 
 pub fn find_default_version(config: &PactupConfig) -> Option<Version> {
   if let Ok(version_path) = config.default_version_dir().canonicalize() {
-    let file_name = version_path.parent()?.file_name()?;
+    let file_name = version_path.file_name()?;
+    info!("Found default version: {:?}", file_name);
     Version::from_str(file_name.to_str()?).ok()?.into()
   } else {
-    Some(Version::Alias("default".into()))
+    info!("No default version found");
+    None
   }
 }
